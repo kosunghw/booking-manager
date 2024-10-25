8a3aa9df0a1a405300802a69432790acd9772804
Sung <sung272012@hotmail.com>
2024-10-24 22:05:29 -0400

    Front End Setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 
 function App() {
+  const [userLogged, setUserLogged] = useState(false);
+
   return (
     <>
       <h1>Welcome to Booking Manager</h1>
