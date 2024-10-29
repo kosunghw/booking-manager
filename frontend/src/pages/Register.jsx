34fc1c48586541d6241343bc5a49a57d556a821e
Sung <sung272012@hotmail.com>
2024-10-28 21:57:49 -0400

    Add simple registration form

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,23 @@
+import { useState } from 'react';
+
 export default function Register() {
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
   return (
     <>
       <h1>Welcome to Register</h1>
+      <section>
+        <form method='POST'>
+          <label type='text' htmlFor='username'>
+            username
+          </label>
+          <input name='username' id='username' />
+          <label htmlFor='password'>password</label>
+          <input type='password' name='password' id='password' />
+          <button type='submit'>Register</button>
+        </form>
+      </section>
     </>
   );
 }
