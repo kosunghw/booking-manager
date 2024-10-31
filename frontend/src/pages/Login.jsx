61427ca6e9da015602651e8cc474e6d3a60be45e
Sung <sung272012@hotmail.com>
2024-10-31 18:33:03 -0400

    Add user register and login using fetch

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,65 @@
+import { useState } from 'react';
+
 export default function Login() {
+  const [formValue, setFormValue] = useState({
+    username: '',
+    password: '',
+  });
+  const [message, setMessage] = useState('');
+
+  const handleInput = (e) => {
+    const { name, value } = e.target;
+    setFormValue({ ...formValue, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const allInputvalue = {
+      username: formValue.username,
+      password: formValue.password,
+    };
+
+    let res = await fetch('http://localhost:5000/api/users/login', {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(allInputvalue),
+    });
+
+    let resjson = await res.json();
+    console.log(resjson);
+    if (res.status === 200) {
+      setMessage(resjson.success);
+      console.log('log in successful');
+    } else {
+      setMessage('Some error occurred');
+    }
+    // console.log(allInputvalue);
+  };
   return (
     <>
       <h1>Welcome to Login</h1>
+      <section>
+        <form onSubmit={handleSubmit}>
+          <label type='text' htmlFor='username'>
+            username
+          </label>
+          <input
+            name='username'
+            id='username'
+            value={formValue.username}
+            onChange={handleInput}
+          />
+          <label htmlFor='password'>password</label>
+          <input
+            type='password'
+            name='password'
+            id='password'
+            value={formValue.password}
+            onChange={handleInput}
+          />
+          <button type='submit'>Login</button>
+        </form>
+      </section>
     </>
   );
 }
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,20 +1,71 @@
 import { useState } from 'react';
 
 export default function Register() {
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+  const [formValue, setFormValue] = useState({
+    username: '',
+    password: '',
+    confirmPassword: '',
+  });
+  const [message, setMessage] = useState('');
+
+  const handleInput = (e) => {
+    const { name, value } = e.target;
+    setFormValue({ ...formValue, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const allInputvalue = {
+      username: formValue.username,
+      password: formValue.password,
+    };
+
+    let res = await fetch('http://localhost:5000/api/users', {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(allInputvalue),
+    });
+
+    let resjson = await res.json();
+    if (res.status === 201) {
+      setMessage(resjson.success);
+    } else {
+      setMessage('Some error occurred');
+    }
+    // console.log(allInputvalue);
+  };
 
   return (
     <>
       <h1>Welcome to Register</h1>
       <section>
-        <form method='POST'>
+        <form onSubmit={handleSubmit}>
           <label type='text' htmlFor='username'>
             username
           </label>
-          <input name='username' id='username' />
+          <input
+            name='username'
+            id='username'
+            value={formValue.username}
+            onChange={handleInput}
+          />
           <label htmlFor='password'>password</label>
-          <input type='password' name='password' id='password' />
+          <input
+            type='password'
+            name='password'
+            id='password'
+            value={formValue.password}
+            onChange={handleInput}
+          />
+          <label htmlFor='confirmPassword'>confirm password</label>
+
+          <input
+            type='password'
+            name='confirmPassword'
+            id='confirmPassword'
+            value={formValue.confirmPassword}
+            onChange={handleInput}
+          />
           <button type='submit'>Register</button>
         </form>
       </section>
