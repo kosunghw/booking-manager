ae1fb303ee748d5604f98aab2a4d6bd02ddaafd9
Sung <sung272012@hotmail.com>
2024-11-03 21:53:43 -0500

    Add route to delete room

--- backend/routes/rooms.js
+++ backend/routes/rooms.js
@@ -2,30 +2,17 @@ const express = require('express');
 const router = express.Router();
 const roomController = require('../controllers/roomController');
 const passport = require('passport');
+const isAuth = require('../middleware/auth');
 
 router.get('/', (req, res) => {
   res.send('ROOM ROUTER');
 });
 
 // create a new room (POST)
-router.post(
-  '/',
-  (req, res, next) => {
-    console.log('=== Session Debug ===');
-    console.log('SessionID:', req.sessionID);
-    console.log('Session:', req.session);
-    console.log('Session Passport:', req.session.passport);
-    console.log('Is authenticated:', req.isAuthenticated());
-    console.log('User:', req.user);
+router.post('/', isAuth, roomController.createRoom);
 
-    if (!req.isAuthenticated()) {
-      return res.status(401).json({ message: 'Not authenticated' });
-    }
-    next();
-  },
-  roomController.createRoom
-);
+router.get('/my-rooms', isAuth, roomController.getRoomByUserId);
 
-router.get('/:userId');
+router.delete('/:roomId', isAuth, roomController.deleteRoom);
 
 module.exports = router;
